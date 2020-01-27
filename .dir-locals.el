ba54f2aa5ddeadaff152cce17f8d48483ed84243
Sébastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2020-01-27 14:11:37 +0100

    Remove the .envrc and .dir-locals.el files

--- .dir-locals.el
+++
@@ -1,8 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil
-  (projectile-project-run-cmd . "jekyll serve"))
- (magit-status-mode
-  (magit-todos-exclude-globs "assets/")))
-
